66c69f22be35162fb2c7af2baec555e2c06c040a
JonahL <103224842+JLesa25@users.noreply.github.com>
2022-08-12 15:22:42 -0600

    Attempt to Commit

--- message_maker.js
+++ message_maker.js
@@ -4,7 +4,19 @@ const messageCenter = [`Sometimes life gives you lemons, whatever it gave you th
                        `The fact that you're looking for this probably means it isn't going all that well.`,
                         `Good luck! You for sure need it`,
                         `Well, I don't know that you can do it, but it'd be super cool if you did`,
+<<<<<<< Updated upstream
                         `When the going gets tough, we'll see how you do.`]
+=======
+                        `When the going gets tough, we'll see how you do I guess.`]
+
+// Happy Message Array
+const happyMessageCenter = ['Today is your day!',
+                            `You can do anything you set your mind to!`,
+                            `Let this be what your sign to go face the day. You've got this`,
+                            `You're more than capable of doing the hard things!`,
+                            `You're too boss to let something stop you today!`,
+                            `Keep your head high, otherwise your crown will fall`]
+>>>>>>> Stashed changes
 
 // Function to bring out a random message
 const linePicker = (arr) => {
@@ -12,4 +24,12 @@ const linePicker = (arr) => {
     return messageCenter[generator]
 };
 
+<<<<<<< Updated upstream
 console.log(`Your unhelpful, sarcastic quote of the day is "${linePicker(messageCenter)}"`);
+=======
+
+
+console.log(`Your unhelpful, sarcastic quote of the day is "${linePicker(sarcasticMessageCenter)}"`);
+
+console.log(`Your helpful, uplifting quote of the day is "${linePicker(happyMessageCenter)}"`);
+>>>>>>> Stashed changes
